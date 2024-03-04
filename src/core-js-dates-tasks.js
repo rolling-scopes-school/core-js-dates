b2a9436b1f41db932ca9115eb899d69790dc1385
Mikhail Aleinik <6424156@tut.by>
2024-03-04 15:43:45 +0300

    fix: updated getWeekNumberByDate description

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -150,6 +150,7 @@ function getCountWeekendsInMonth(/* month, year */) {
 /**
  * Returns the week number of the year for a given date.
  * The first week is the one that falls on January 1.
+ * The first day of the week is Monday.
  *
  * @param {Date} date - The date for which to find the week number.
  * @return {number} - The week number of the year.
